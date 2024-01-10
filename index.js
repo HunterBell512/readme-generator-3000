4f6569e7c084345295199c76ca5f1ca5046a5523
HunterBell512 <bellhunter512@gmail.com>
2024-01-10 04:02:52 -0500

    added to prompt and got basic md generated

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
-// TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js')
 
-// TODO: Create an array of questions for user input
 const questions = [
     'What is the name of your application?',
     'What does your application do?',
     'Add an installation step?',
     'What is your application used for?',
-    'Who are the contributors of the project?'
+    'Who are the contributors of the project?',
+    'Which license would you like to use?',
+    'Type a command you can use to test your application',
+    'What is your GitHub username?',
+    'What is your email address?'
 ];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) throw err;
+        console.log('readme successfully generated to the output folder!');
+    })
+}
 
-// TODO: Create a function to initialize app
 function init() {
     inquirer.registerPrompt('loop', require('inquirer-loop')(inquirer));
 
@@ -44,7 +50,22 @@ function init() {
                 type: 'rawlist',
                 name: 'license',
                 message: questions[5],
-                choices: ['MIT', 'Apache 2.0', 'GPL v3', 'MPL 2.0', 'BSD 3']
+                choices: ['MIT', 'Apache 2.0', 'GPL v3', 'MPL 2.0', 'BSD 3.0']
+            },
+            {
+                type: 'input',
+                name: 'test',
+                message: questions[6]
+            },
+            {
+                type: 'input',
+                name: 'github',
+                message: questions[7]
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: questions[8]
             },
             {
                 type: 'loop',
@@ -53,13 +74,14 @@ function init() {
                 questions: [
                     {
                         type: 'name',
-                        name: 'step',
+                        name: 'steps',
                         message: 'Add the step:'
                     }
                 ]
             }
         ]).then((answers) => {
             console.log(answers);
+            writeToFile('./output/README.md', generateMarkdown(answers));
         });
 }
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,65 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  return 0;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  return 0;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  return 0;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  let stepNum = 0;
   return `# ${data.title}
 
+## Description
+${data.description}
+  
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Preview
+![preview]('./assets/images/preview.png')
+  
+## Installation
+${data.installation.map(elem => {
+  stepNum += 1;
+  return ((stepNum) + ". " + elem["steps"]);
+}).join('\n')}
+  
+## Usage
+${data.usage}
+
+## Tests
+Here are some tests to run to verify a proper install:
+\`\`\`bash
+${data.test}
+\`\`\`
+
+## Contributors
+${data.contributions}
+
+## Questions
+If you have any questions, please reach out!
+- Github: [${data.github}](https://github.com/${data.github})
+- Email: [${data.email}](mailto:${data.email})
+
+## License
+${renderLicenseSection(data.license)}
 `;
 }
 
